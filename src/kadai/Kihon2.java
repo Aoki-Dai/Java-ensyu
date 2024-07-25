26e7881fffea2b5c55eb5cf0fc9d0e640da9009c
Aoki-Dai <108746684+Aoki-Dai@users.noreply.github.com>
2024-07-25 18:00:10 +0900

    chore： コメントの追加

--- src/kadai/Kihon2.java
+++ src/kadai/Kihon2.java
@@ -15,22 +15,24 @@ import java.io.IOException; // 入出力例外を処理するためのクラス
 public class Kihon2 {
 	public static void main(String[] args) throws IOException {
 		// TODO 自動生成されたメソッド・スタブ
-		FileReader fr = new FileReader("kihon2in.txt");
-		BufferedReader br = new BufferedReader(fr);
-		FileWriter fw = new FileWriter("kihon2out.txt");
-		BufferedWriter bw = new BufferedWriter(fw);
+		// ファイルからテキストデータを読み，文字数を数える
+		FileReader fr = new FileReader("kihon2in.txt");// 入力ファイル
+		BufferedReader br = new BufferedReader(fr);// バッファリングされた文字ストリーム
+		FileWriter fw = new FileWriter("kihon2out.txt");// 出力ファイル
+		BufferedWriter bw = new BufferedWriter(fw);// バッファリングされた文字ストリーム
 		String s;
 		int i = 0;
-		while ((s = br.readLine()) != null) {
-			System.out.println("”" + s + "”" + "の文字数は" + s.length() + "です。");
-			bw.write("”" + s + "”" + "の文字数は" + s.length() + "です。");
-			bw.newLine();
-			i += 1;
+		// ファイルから1行ずつ読み，文字数を数える
+		while ((s = br.readLine()) != null) {// ファイルから1行ずつ読む
+			System.out.println("”" + s + "”" + "の文字数は" + s.length() + "です。");// コンソールに出力
+			bw.write("”" + s + "”" + "の文字数は" + s.length() + "です。");// 出力ファイルに書く
+			bw.newLine();// 改行
+			i += 1;// 行数をカウントアップ
 		}
 		System.out.println("全部で" + i + "行です。");
-		bw.write("全部で" + i + "行です。");
-		bw.newLine();
-		br.close();
-		bw.close();
+		bw.write("全部で" + i + "行です。");// 出力ファイルに書く
+		bw.newLine(); // 改行
+		br.close();// 入力ファイルを閉じる
+		bw.close();// 出力ファイルを閉じる
 	}
 }
