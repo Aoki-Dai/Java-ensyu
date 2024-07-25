446a3596f7ae01d175784056d8f2468fa3b93948
Aoki-Dai <108746684+Aoki-Dai@users.noreply.github.com>
2024-07-25 23:11:10 +0900

    chore: Hatten6Sample.javaにコメントを追加

--- src/kadai/Hatten6Sample.java
+++ src/kadai/Hatten6Sample.java
@@ -9,11 +9,11 @@ import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
 public class Hatten6Sample {
-	
+
 	private JFrame jf = null;
 	private JPanel cp = null;
 	private int xStart = 0, yStart = 0;
-	
+
 	private JFrame getJFrame() {
 		if (jf == null) {
 			jf = new JFrame();
@@ -25,11 +25,11 @@ public class Hatten6Sample {
 		}
 		return jf;
 	}
-	
+
 	private JPanel getJContentPane() {
 		if (cp == null) {
 			cp = new JPanel();
-			cp.addMouseListener(new MyMouseAdapter());
+			cp.addMouseListener(new MyMouseAdapter());// マウスリスナーを登録
 		}
 		return cp;
 	}
@@ -40,15 +40,15 @@ public class Hatten6Sample {
 			int xEnd, yEnd;
 			// コンテントペーンのグラフィックオブジェクトを取得
 			Graphics g = cp.getGraphics();
-			xEnd = e.getX();
-			yEnd = e.getY();
+			xEnd = e.getX();// マウスのX座標を取得
+			yEnd = e.getY();// マウスのY座標を取得
 			// (xStart, yStart)と(xEnd, yEnd)の2点間に直線を引く
 			g.drawLine(xStart, yStart, xEnd, yEnd);
-			xStart = xEnd;
-			yStart = yEnd;
+			xStart = xEnd;// 次の直線の始点(x)を設定
+			yStart = yEnd;// 次の直線の始点(y)を設定
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		SwingUtilities.invokeLater(new Runnable() {
